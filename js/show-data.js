755da99ea15849e8b94a2899237a0930d0f4b4e1
tcrurav <tiburcio.cruz@gmail.com>
2024-11-07 21:45:48 +0000

    fix some details

--- js/show-data.js
+++ js/show-data.js
@@ -1,14 +1,14 @@
 // DECOMMENT: STEP 9: Create a function to show the data stored in LocalStorage
-// function showData(){
-//   const NAME = localStorage.getItem("name");
-//   document.getElementById("name").innerHTML = NAME;
+function showData(){
+  const NAME = localStorage.getItem("name");
+  document.getElementById("name").innerHTML = NAME;
 
-//   const SURNAME = localStorage.getItem("surname");
-//   document.getElementById("surname").innerHTML = SURNAME;
+  const SURNAME = localStorage.getItem("surname");
+  document.getElementById("surname").innerHTML = SURNAME;
 
-//   const EMAIL = localStorage.getItem("email");
-//   document.getElementById("email").innerHTML = EMAIL;
-// }
+  const EMAIL = localStorage.getItem("email");
+  document.getElementById("email").innerHTML = EMAIL;
+}
 
 // DECOMMENT: STEP 10: Don't forget to call the function that shows the data stored in LocalStorage
-// showData();
+showData();
--- js/validate.js
+++ js/validate.js
@@ -1,47 +1,47 @@
 // DECOMMENT: STEP 3: Create a function that listens on a submit even of the created form
-// function listenForValidation() {
-//   const PERSONAL_DATA_FORM = document.getElementById("personal-data-form");
-//   PERSONAL_DATA_FORM.addEventListener("submit", validatePersonalDataForm);
-// }
+function listenForValidation() {
+  const PERSONAL_DATA_FORM = document.getElementById("personal-data-form");
+  PERSONAL_DATA_FORM.addEventListener("submit", validatePersonalDataForm);
+}
 
 
 // DECOMMENT: STEP 5: Create a function which validates the data sent by the form when the user clicks on submit button
-// function validatePersonalDataForm(e) {
+function validatePersonalDataForm(e) {
 
-//   const NAME = e.target.name.value;
-//   const SURNAME = e.target.surname.value;
-//   const EMAIL = e.target.email.value;
+  const NAME = e.target.name.value;
+  const SURNAME = e.target.surname.value;
+  const EMAIL = e.target.email.value;
 
-//   let valid = true;
+  let valid = true;
 
-//   if (!NAME) {
-//     document.getElementById("form-name").style.visibility = "visible";
-//     valid = false;
-//   } else document.getElementById("form-name").style.visibility = "hidden";
+  if (!NAME) {
+    document.getElementById("form-name").style.visibility = "visible";
+    valid = false;
+  } else document.getElementById("form-name").style.visibility = "hidden";
 
-//   if (!SURNAME) {
-//     document.getElementById("form-surname").style.visibility = "visible";
-//     valid = false;
-//   } else document.getElementById("form-surname").style.visibility = "hidden";
+  if (!SURNAME) {
+    document.getElementById("form-surname").style.visibility = "visible";
+    valid = false;
+  } else document.getElementById("form-surname").style.visibility = "hidden";
 
-//   if (!EMAIL) {
-//     document.getElementById("form-email").style.visibility = "visible";
-//     valid = false;
-//   } else document.getElementById("form-email").style.visibility = "hidden";
+  if (!EMAIL) {
+    document.getElementById("form-email").style.visibility = "visible";
+    valid = false;
+  } else document.getElementById("form-email").style.visibility = "hidden";
 
-//   if (!valid) {
-//     e.preventDefault();
-//   } else {
-//     saveData(NAME, SURNAME, EMAIL);
-//   }
-// }
+  if (!valid) {
+    e.preventDefault();
+  } else {
+    saveData(NAME, SURNAME, EMAIL);
+  }
+}
 
 // DECOMMENT: STEP 6: Create a function to save the form data in LocalStorage
-// function saveData(name, surname, email) {
-//   localStorage.setItem("name", name);
-//   localStorage.setItem("surname", surname);
-//   localStorage.setItem("email", email);
-// }
+function saveData(name, surname, email) {
+  localStorage.setItem("name", name);
+  localStorage.setItem("surname", surname);
+  localStorage.setItem("email", email);
+}
 
 // DECOMMENT: STEP 4: Don't forget to call the function to listen the submit event
-// listenForValidation();
+listenForValidation();
